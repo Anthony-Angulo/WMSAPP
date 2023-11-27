4b87346a6d8016a26a37033c54f0b454839820a5
Anthony Angulo <anthony.angulo@superchivas.com.mx>
2023-11-27 15:37:43 -0800

    cambios para menudeos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@ export const environment = {
   production: false,
   apiCRM: 'http://crm.ccfnweb.com.mx/apicrm/public/api',
   apiWMS: 'http://crm.ccfnweb.com.mx/apiwms/public/api',
-  apiSAPR: 'http://192.168.115.35:8090', 
+  apiSAPR: 'http://192.168.115.8:8070', 
   apiCCFN: 'http://apiccfn.ccfnweb.com.mx/api', 
   // apiCCFN: 'http://localhost:5005/api',
   // apiSAP: 'http://192.168.0.10:8886',
-  apiSAP: 'http://192.168.0.10:8889',
+  apiSAP: 'http://192.168.115.8:8070',
   update: 'http://crm.ccfnweb.com.mx/update.xml' 
 };
 
